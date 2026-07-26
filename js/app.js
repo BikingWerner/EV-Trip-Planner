53e58464fc238fce7715a10bf40c0d8978d52a0f
BikingWerner <biking.werner@gmail.com>
2026-07-26 21:27:49 +0200

    Enhance results display with trip information
    Updated the results display to show trip information including from, to, distance, detour, and power.

--- js/app.js
+++ js/app.js
@@ -15,9 +15,23 @@ document.addEventListener(
             "click",
             function () {
 
+const from = document.getElementById("from").value;
+const to = document.getElementById("to").value;
+const distance = document.getElementById("distance").value;
+const detour = document.getElementById("detour").value;
+const power = document.getElementById("power").value;
 
-                document.getElementById("results").innerHTML =
-                    "Knop werkt!";
+
+document.getElementById("results").innerHTML =
+    `
+    <strong>Trip information</strong><br><br>
+
+    From: ${from}<br>
+    Destination: ${to}<br>
+    Charging interval: ${distance} km<br>
+    Maximum detour: ${detour} km<br>
+    Minimum charging power: ${power} kW
+    `;
 
 
             }
