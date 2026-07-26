b1eea8b9f4a2acaea68021db737b68504ac8fac2
BikingWerner <biking.werner@gmail.com>
2026-07-26 21:37:04 +0200

    Add route module

---
+++ js/route.js
@@ -0,0 +1,12 @@
+async function calculateRoute(from, to) {
+
+    console.log("Calculating route:");
+    console.log("From:", from);
+    console.log("To:", to);
+
+
+    return {
+        message: "Route calculation ready"
+    };
+
+}
