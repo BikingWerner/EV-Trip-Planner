d814d5068dc3064504e9a059f6cbe23be882c3a3
BikingWerner <biking.werner@gmail.com>
2026-07-26 21:23:17 +0200

    Add button click event to display results message

--- js/app.js
+++ js/app.js
@@ -2,7 +2,27 @@ document.addEventListener(
     "DOMContentLoaded",
     function () {
 
+
         initializeMap();
+
+
+        const button = document.getElementById(
+            "calculateButton"
+        );
+
+
+        button.addEventListener(
+            "click",
+            function () {
+
+
+                document.getElementById("results").innerHTML =
+                    "Knop werkt!";
+
+
+            }
+        );
+
 
     }
 );
