72f4a6bf2419a1a9242d0c7f83d068d8082b7fba
BikingWerner <biking.werner@gmail.com>
2026-07-26 21:14:14 +0200

    Refactor map initialization and cleanup
    Removed commented code and unnecessary variables.

--- js/map.js
+++ js/map.js
@@ -1,64 +1,20 @@
-```javascript
-// EV Trip Planner
-// Map module
-// Version 0.2
-
-
 let map;
-let routeLayer;
 
-
-// Initialiseer de kaart
 
 function initializeMap() {
 
-    map = L.map('map').setView(
-        [50.8503, 4.3517],   // België (centrum)
+    map = L.map("map").setView(
+        [50.8503, 4.3517],
         8
     );
 
 
     L.tileLayer(
-        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+        "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
         {
             attribution:
-            '&copy; OpenStreetMap contributors'
+            "&copy; OpenStreetMap contributors"
         }
     ).addTo(map);
 
-
-    routeLayer = L.layerGroup().addTo(map);
-
 }
-
-
-
-// Teken een route op de kaart
-
-function drawRoute(coordinates) {
-
-
-    // verwijder vorige route
-
-    routeLayer.clearLayers();
-
-
-    const route = L.polyline(
-        coordinates,
-        {
-            weight: 5
-        }
-    );
-
-
-    route.addTo(routeLayer);
-
-
-    // zoom kaart naar route
-
-    map.fitBounds(
-        route.getBounds()
-    );
-
-}
-```
