d8d5c2c8fce7a73a13ebb5196240ef6be9e3f6f0
BikingWerner <biking.werner@gmail.com>
2026-07-29 17:42:15 +0200

    Fix typo in console.log statement

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.ORS_API_KEY = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImIzM2U2Y2E1NDc0YjQ0MDc4NjFiZTY2YmE2MDQyOTdkIiwiaCI6Im11cm11cjY0In0";
 
-cconsole.log("CONFIG GELADEN");
+console.log("CONFIG GELADEN");
 console.log(window.ORS_API_KEY);
